a3a43023c61eecb6aacd3a1eb555978b3c488721
tomer <tomerguttman@campus.technion.ac.il>
2025-01-19 13:23:37 +0200

    starting customMalloc

--- customAllocator.cpp
+++ customAllocator.cpp
@@ -5,13 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <malloc.h>
+#include <iostream>
+#include "customAllocator.h"
 
 using namespace std;
 
-void* customMalloc(size_t size){
+void* customMalloc(size_t size) {
     if(size <= 0){
         cerr << "<malloc error>: passed nonpositive size" << endl;
         return nullptr;
+    }
+
+    size = ALIGN_TO_MULT_OF_4(size);
+    block *new_block;
+
+    if(!blockList){
+        new_block = moreMemmory(size);
     }
 }
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 // Created by tomer on 17/01/2025.
 //
 
+#include "customAllocator.h"
+
 int main() {
   return 0;
 }
