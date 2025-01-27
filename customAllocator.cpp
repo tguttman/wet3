7d7142a9d9820d97b4ee974683d806ecc1aa3656
Hagai <hshlasky@gmail.com>
2025-01-27 17:32:42 +0200

    Fixing a few issues in the basic assignment, such as:  - If trying to allocate less than the block's size, the block is splitted in two blocks, one with the requested size and the other with the remaining size. - If the block is freed, the allocator will merge it with the next block if it is free. - When possible, reduce the program break to free memory to the OS.
    In addition, started working on bonus assignment by creating a class named slice to hold the parts of the partitioned heap + implemented customMTMalloc()

--- customAllocator.cpp
+++ customAllocator.cpp
@@ -7,86 +7,199 @@
 #include <unistd.h>
 #include "customAllocator.h"
 #include <string.h>
+#include <errno.h>
+#include <cassert>
 
 using namespace std;
-Block* blockList;
-// Finds free memory in the block list
-Block *findFreeMemory(Block **last, size_t size) {
-    Block *current = blockList;
+
+Block_t* blockList = nullptr;
+Block_t* lastBlock = nullptr;
+Slice_t* heap = nullptr;
+ReadersWritersLock heap_lock;
+size_t num_slices = 0;
+
+void print_blockList() {
+    Block_t* current = blockList;
+    cout << "-------blockList status:--------" << endl;
+    int i = 1;
     while (current) {
-        if (current->free && current->size >= size) {
-            return current;
-        }
-        *last = current;
+        cout << "Block " << i << ": ";
+        current->print();
         current = current->next;
+        i++;
+    }
+}
+
+void addBlock(Block_t** list, Block_t* new_block) {
+    new_block->next = *list;
+    *list = new_block;
+    if (!new_block->next) 
+        lastBlock = new_block;
+    
+}
+
+void removeBlock(Block_t** list, Block_t* block) {
+    Block_t** current = list;
+    while (*current && *current != block) {
+        current = &(*current)->next;
+    }
+    if (*current) {
+        *current = block->next;
+        if (!block->next) {
+            blockList = (current == list) ? nullptr : *current;
+        }
+    }
+}
+
+Block_t* findFreeBlock(Block_t* list, size_t size) {
+    while (list) {
+        if (list->free && list->size >= size) {
+            return list;
+        }
+        list = list->next;
     }
     return nullptr;
 }
 
-// Adds memory in the end of the block list
-Block *moreMemory(Block *last, size_t size) {
-    Block *new_block = static_cast<Block*>(sbrk(0));
-    void *requsted_memory = sbrk(size + sizeof(Block));
-    if (requsted_memory == (void *)-1) {
-        if (errno == ENOMEM) {
-            Block* current = blockList;
-            Block *next = current->next;
-            while (current) {
-                customFree(current + 1);
-                current = next;
-                next = current->next;
-            }
-            cerr << "<sbrk/brk error>: out of memory" << endl;
-            exit(1);
-        } /*else if (errno == EINVAL) {
-            printf("Error: Invalid increment value (EINVAL)\n");
-        } else {
-            printf("Error: Unknown error occurred (%s)\n", strerror(errno));
-        }*/
+Block_t* allocateBlock(size_t size) {
+    Block_t* new_block = (Block_t*)sbrk(sizeof(Block_t));
+    if (new_block == (Block_t*)-1) {
+        cerr << "<sbrk/brk error>: out of memory" << endl;
         return nullptr;
     }
-
-    if (last) {
-        last->next = new_block;
-    }
     new_block->size = size;
-    new_block->next = nullptr;
     new_block->free = false;
+    new_block->memory = sbrk(size);
+    if (new_block->memory == (void*)-1) {
+        sbrk(-sizeof(Block_t));
+        cerr << "<sbrk/brk error>: out of memory" << endl;
+        return nullptr;
+    }
+    new_block->next = nullptr;
     return new_block;
 }
 
 // Returns pointer to memory in the requested size.
 // New_block contains the information about the memory.
 void* customMalloc(size_t size) {
+    if (size <= 0) {
+        cerr << "<malloc error>: passed nonpositive size" << endl;
+        return nullptr;
+    }
+
+    size = ALIGN_TO_MULT_OF_4(size);
+    Block_t* new_block;
+
+    if (!blockList) { // In case it's the first allocation
+        new_block = allocateBlock(size);
+        if (!new_block) {
+            return nullptr;
+        }
+        blockList = new_block;
+        lastBlock = new_block;
+    } else { // If it's not the first allocation
+        new_block = findFreeBlock(blockList, size);
+        if (!new_block) { // If the list is full, get more memory
+            new_block = allocateBlock(size);
+            if (!new_block) {
+                return nullptr;
+            }
+            addBlock(&blockList, new_block);
+        } else { // If an empty block was found in the blockList
+            if (new_block->size > size) {
+                Block_t* remaining_block = (Block_t*)sbrk(sizeof(Block_t));
+                if (remaining_block == (Block_t*)-1) {
+                    cerr << "<sbrk/brk error>: out of memory" << endl;
+                    return nullptr;
+                }
+                remaining_block->size = new_block->size - size;
+                remaining_block->free = true;
+                remaining_block->memory = (char*)new_block->memory + size;
+                remaining_block->next = new_block->next;
+                new_block->size = size;
+                new_block->next = remaining_block;
+                if (!remaining_block->next) {
+                    lastBlock = remaining_block;
+                }
+            }
+            new_block->free = false;
+        }
+    }
+    return new_block->memory; // Pointer to the allocated memory
+}
+
+// Thread-safe heap allocation in a circular manner
+void* customMTMalloc(size_t size) {
     if(size <= 0){
         cerr << "<malloc error>: passed nonpositive size" << endl;
         return nullptr;
     }
 
     size = ALIGN_TO_MULT_OF_4(size);
-    Block *new_block;
+    Block_t *new_block;
 
-    if(!blockList){     // In case it's the first allocation
-        new_block = moreMemory(NULL, size);
-        if(!new_block) {
-            return nullptr;
-        }
-        blockList = new_block;
-    }
-    else {      // If it's not the first allocation
-        Block *last_block = blockList;
-        new_block = findFreeMemory(&last_block, size);
-        if(!new_block) {    // If the list is full, get more memory
-            new_block = moreMemory(last_block, size);
-            if(!new_block) {
-                return nullptr;
+    heap_lock.read_lock();
+    static Slice_t* current_slice = heap;
+    for (size_t i = 0; i < num_slices; ++i) {
+        if (mutex_trylock(&current_slice->lock) == 0) {
+            new_block = findFreeBlock(current_slice->block_list, size);
+            if (new_block) {
+                if (new_block->size > size) {
+                    Block_t* remaining_block = (Block_t*)sbrk(sizeof(Block_t));
+                    if (remaining_block == (Block_t*)-1) {
+                        cerr << "<sbrk/brk error>: out of memory" << endl;
+                        return nullptr;
+                    }
+                    remaining_block->size = new_block->size - size;
+                    remaining_block->free = true;
+                    remaining_block->memory = (char*)new_block->memory + size;
+                    remaining_block->next = new_block->next;
+                    new_block->size = size;
+                    new_block->next = remaining_block;
+                    if (!remaining_block->next) {
+                        lastBlock = remaining_block;
+                    }
+                }
+                new_block->free = false;
+                mutex_unlock(&current_slice->lock);
+                heap_lock.read_unlock();
+                return new_block->memory;
             }
+            mutex_unlock(&current_slice->lock);
         }
-        else {      // If an empty block was found in the blockList
-            new_block->free = false;
-        }
+        current_slice = current_slice->next ? current_slice->next : heap;
     }
-    return (new_block + 1);     // Pointer to the memory after new_block
+    heap_lock.read_unlock();
+
+    heap_lock.write_lock();
+    Slice_t* new_slice = (Slice_t*)sbrk(sizeof(Slice_t));
+    if (new_slice == (Slice_t*)-1) {
+        cerr << "<sbrk/brk error>: out of memory" << endl;
+        heap_lock.write_unlock();
+        return nullptr;
+    }
+    *new_slice = Slice_t();
+    new_slice->next = heap;
+    heap = new_slice;
+    num_slices++;
+    mutex_lock(&new_slice->lock);
+    heap_lock.write_unlock();
+
+    new_block = new_slice->block_list;
+    new_block->free = false;
+    mutex_unlock(&new_slice->lock);
+    return new_block->memory;
+}
+
+Block_t *findBlock(void *ptr) {
+    Block_t* current = blockList;
+    while (current) {
+        if (current->memory == ptr) {
+            return current;
+        }
+        current = current->next;
+    }
+    return nullptr;
 }
 
 // Frees memory
@@ -95,23 +208,40 @@ void customFree(void* ptr) {
         cerr << "<free error>: passed null pointer" << endl;
         return;
     }
-
+    if (!blockList) {
+        return;
+    }
     // Pointer to the information block
-    auto *blockToFree = static_cast<Block *>(ptr) - 1;
+    auto* blockToFree = findBlock(ptr);
+    if (!blockToFree) 
+        return;
+    
     blockToFree->free = true;
 
-    if (!blockToFree->next) {   // If it's the last block in the list
-        sbrk(-1 * (sizeof(Block) + blockToFree->size));
+    // Merge adjacent free blocks
+    Block_t* current = blockList;
+    while (current) {
+        if (current->free && current->next && current->next->free) {
+            current->size += current->next->size + sizeof(Block_t);
+            current->next = current->next->next;
+            if (!current->next)
+                lastBlock = current;
+        } else {
+            current = current->next;
+        }
     }
-    else {      // If the freed memory is the center of the list
-        Block* current = blockList;
-        while (current) {
-            if (current->free && current->next && current->next->free) {
-                current->size += sizeof(Block) + current->next->size;
-                current->next = current->next->next;
-            } else {
-                current = current->next;
-            }
+    // Reduce program break if possible
+    if (blockList && blockList->free) {
+        bool last = !blockList->next;
+        if (sbrk(-(blockList->size + sizeof(Block_t))) == (void*)-1) {
+            cerr << "<sbrk/brk error>: unable to reduce program break" << endl;
+            exit(1);
+        }
+        if (last) {
+            blockList = nullptr;
+            lastBlock = nullptr;
+        } else {
+            removeBlock(&blockList, blockList);
         }
     }
 }
@@ -134,6 +264,7 @@ void* customCalloc(size_t nmemb, size_t size) {
 
 void* customRealloc(void* ptr, size_t size) {
     // Ptr is null: malloc
+    size = ALIGN_TO_MULT_OF_4(size);
     if (!ptr) {
         if (size == 0) return nullptr;
         return customMalloc(size);
@@ -145,7 +276,7 @@ void* customRealloc(void* ptr, size_t size) {
         return ptr;     // Weird but that's what they asked
     }
 
-    auto *block = (Block *)(ptr) - 1;
+    auto *block = findBlock(ptr);
 
     // If the requested memory is at the same size of the old memory: do nothing
     if (block->size == size) {
@@ -154,40 +285,47 @@ void* customRealloc(void* ptr, size_t size) {
 
     // Temporary memory to hold ptr content
     void *temp = customMalloc(size);
-    memcpy(temp, ptr, size);
+    Block_t *new_block = findBlock(temp);
+    size_t min_size = min(block->size, size);
+    memcpy(new_block->memory, ptr, min_size);
 
     customFree(ptr);    // Freeing ptr so customMalloc will see it as empty memory
-    void *new_ptr = customMalloc(size);
+    /*void *new_ptr = customMalloc(size);
     if (!new_ptr) {
         return nullptr;
     }
     memcpy(new_ptr, temp, size);
-    customFree(temp);
-    return new_ptr;
+    customFree(temp);*/
+    return temp;
+}
 
-    /*
-    // If new size is bigger: replace with a bigger size memory
-    if (block->size < size) {
-        // * * * we should copy ptr content to temp, free ptr, and search for memory
-        //at the requested size * * * //
-        void *new_ptr = customMalloc(size);
-        if (!new_ptr) {
-            return nullptr;
+void heapCreate() {
+    for (int i = 0 ; i < 8 ; i++) {
+        Slice_t* new_slice = (Slice_t*)sbrk(sizeof(Slice_t));
+        if (new_slice == (Slice_t*)-1) {
+            cerr << "<sbrk/brk error>: out of memory" << endl;
+            heapKill();
+            exit(1);
         }
-        memcpy(new_ptr, ptr, block->size);
-        customFree(ptr);
-        return new_ptr;
+        *new_slice = Slice_t();
+        new_slice->next = heap;
+        heap = new_slice;
+        num_slices++;
     }
+}
 
-    // If new size is smaller: replace with smaller size memory
-    if (block->size > size) {
-        customFree(ptr);    // Freeing ptr so customMalloc will see it as empty memory
-        void *new_ptr = customMalloc(size);
-        if (!new_ptr) {
-            return nullptr;
+void heapKill() {
+    Slice_t* current_slice = heap;
+    while (current_slice) {
+        Block_t* current_block = current_slice->block_list;
+        while (current_block) {
+            Block_t* next_block = current_block->next;
+            sbrk(-(current_block->size + sizeof(Block_t)));
+            current_block = next_block;
         }
-        memcpy(new_ptr, temp, size);
-        customFree(temp);
-        return new_ptr;
-    }*/
+        Slice_t* next_slice = current_slice->next;
+        sbrk(-sizeof(Slice_t));
+        current_slice = next_slice;
+        num_slices--;
+    }
 }
--- customAllocator.h
+++ customAllocator.h
@@ -5,9 +5,16 @@
 * do no edit lines below!
 =============================================================================*/
 #include <stddef.h> //for size_t
+#include <list>
+#include <pthread.h>
+#include <cstring>
+#include <cstdint>
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
 
 void* customMalloc(size_t size);
-void customFree(void* ptr);
+void  customFree(void* ptr);
 void* customCalloc(size_t nmemb, size_t size);
 void* customRealloc(void* ptr, size_t size);
 /*=============================================================================
@@ -17,22 +24,110 @@ void* customRealloc(void* ptr, size_t size);
 /*=============================================================================
 * if writing bonus - uncomment lines below
 =============================================================================*/
-// #ifndef __BONUS__
-// #define __BONUS__
-// #endif
-// void* customMTMalloc(size_t size);
-// void customMTFree(void* ptr);
-// void* customMTCalloc(size_t nmemb, size_t size);
-// void* customMTRealloc(void* ptr, size_t size);
+#ifndef __BONUS__
+#define __BONUS__
+#endif
+void* customMTMalloc(size_t size);
+void* customMTFree(void* ptr);
+void* customMTCalloc(size_t nmemb, size_t size);
+void* customMTRealloc(void* ptr, size_t size);
 
-// void heapCreate();
-// void heapKill();
+void heapCreate();
+void heapKill();
 
 /*=============================================================================
 * defines
 =============================================================================*/
 #define SBRK_FAIL (void*)(-1)
 #define ALIGN_TO_MULT_OF_4(x) (((((x) - 1) >> 2) << 2) + 4)
+#define KB (1 << 10)
+#define SLICE_SIZE (4 * KB)
+
+using namespace std;
+void print_blockList();
+inline void mutex_init(pthread_mutex_t* lock) {
+    if (pthread_mutex_init(lock, NULL) != 0) {
+        cerr << "<mutex error>: mutex init failed" << endl;
+        heapKill();
+        exit(1);
+    }
+}
+
+inline void mutex_lock(pthread_mutex_t* lock) {
+    if (pthread_mutex_lock(lock) != 0) {
+        cerr << "<mutex error>: mutex lock failed" << endl;
+        heapKill();
+        exit(1);
+    }
+}
+
+inline int mutex_trylock(pthread_mutex_t* lock) {
+    int res = pthread_mutex_trylock(lock);
+    if (res != 0 && errno != EBUSY) {
+        cerr << "<mutex error>: mutex trylock failed" << endl;
+        heapKill();
+        exit(1);
+    }
+    return res;
+}
+
+inline void mutex_unlock(pthread_mutex_t* lock) {
+    if (pthread_mutex_unlock(lock) != 0) {
+        cerr << "<mutex error>: mutex unlock failed" << endl;
+        heapKill();
+        exit(1);
+    }
+}
+
+inline void mutex_destroy(pthread_mutex_t* lock) {
+    if (pthread_mutex_destroy(lock) != 0) {
+        cerr << "<mutex error>: mutex destroy failed" << endl;
+        heapKill();
+        exit(1);
+    }
+}
+
+class ReadersWritersLock {
+private:
+    uint32_t num_readers = 0;
+    pthread_mutex_t r_lock;
+    pthread_mutex_t w_lock;
+
+public:
+    ReadersWritersLock() {
+        mutex_init(&r_lock);
+        mutex_init(&w_lock);
+    }
+
+    ~ReadersWritersLock() {
+        mutex_destroy(&r_lock);
+        mutex_destroy(&w_lock);
+    }
+
+    void read_lock() {
+        mutex_lock(&r_lock);
+        this->num_readers++;
+        if (this->num_readers == 1)
+            mutex_lock(&w_lock);
+        mutex_unlock(&r_lock);
+    }
+
+    void read_unlock() {
+        mutex_lock(&r_lock);
+        this->num_readers--;
+        if (this->num_readers == 0)
+            mutex_unlock(&w_lock);
+        mutex_unlock(&r_lock);
+    }
+
+    void write_lock() {
+        mutex_lock(&w_lock);
+    }
+
+    void write_unlock() {
+        mutex_unlock(&w_lock);
+    }
+};
 
 /*=============================================================================
 * Block
@@ -41,9 +136,55 @@ void* customRealloc(void* ptr, size_t size);
 typedef struct Block
 {
     size_t size;
-    struct Block* next;
     bool free;
-} Block;
-extern Block* blockList;
+    void* memory; // Pointer to the allocated memory
+    struct Block* next;
+
+    void print() {
+        cout << "size=" << size << ", free=" << free << ", memory=" << memory << ", next=" << next << endl;
+    }
+} Block_t;
+
+typedef struct Slice {
+    size_t free_space;
+    pthread_mutex_t lock;
+    Block_t* block_list;
+    struct Slice* next;
+
+    Slice() : free_space(SLICE_SIZE), block_list(nullptr), next(nullptr) {
+        mutex_init(&lock);
+        Block_t* new_block = (Block_t*)sbrk(sizeof(Block_t));
+        new_block->size = SLICE_SIZE;
+        new_block->free = true;
+        new_block->memory = sbrk(SLICE_SIZE);
+        if (new_block->memory == SBRK_FAIL) {
+            cerr << "<sbrk/brk error>: out of memory" << endl;
+            heapKill();
+            exit(1);
+        }
+        new_block->next = nullptr;
+        block_list = new_block;
+    }
+
+    ~Slice() {
+        mutex_destroy(&lock);
+        Block_t* current = block_list;
+        while (current) {
+            Block_t* next = current->next;
+            sbrk(-(current->size + sizeof(Block_t)));
+            current = next;
+        }
+    }
+} Slice_t;
+
+extern Block_t* blockList;
+extern Block_t* lastBlock;
+extern Slice_t* heap;
+extern size_t num_slices;
+
+void addBlock(Block_t** list, Block_t* new_block);
+void removeBlock(Block_t** list, Block_t* block);
+Block_t* findFreeBlock(Block_t* list, size_t size);
+Block_t* allocateBlock(size_t size);
 
 #endif // CUSTOM_ALLOCATOR_
